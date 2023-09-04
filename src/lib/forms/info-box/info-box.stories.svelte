858c0ab25a7524ca88ae6c176ef958d89923e779
Jason Byrne <jason.byrne@oddball.io>
2023-09-04 16:29:18 -0400

    v0.0.45

---
+++ src/lib/forms/info-box/info-box.stories.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import InfoBox from './info-box.svelte';
+</script>
+
+<Meta title="Forms/InfoBox" component={InfoBox} />
+
+<Story name="Default">
+	<InfoBox value="Some text goes here">Some Label</InfoBox>
+</Story>
+
+<Story name="With Link">
+	<InfoBox value="Some text goes here" href="https://www.google.com">Some Label</InfoBox>
+</Story>
--- src/lib/forms/info-box/info-box.svelte
+++ src/lib/forms/info-box/info-box.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import LinkIcon from '$src/lib/icons/link-icon.svelte';
+	import Icon from '$src/lib/images/icon.svelte';
 	import type { FormFieldSizeOptions } from '$src/lib/index.js';
 	import FormField from '../form-field.svelte';
 	import FormLabel from '../form-label.svelte';
 
 	export let size: FormFieldSizeOptions = 'md';
 	export let value: string;
+	export let href: string | undefined = undefined;
 </script>
 
 <FormField {size}>
@@ -12,17 +15,51 @@
 		<FormLabel><slot /></FormLabel>
 	{/if}
 	<div class="input">
-		{value}
+		{#if href}
+			<span class="icon">
+				<LinkIcon />
+			</span>
+			<a {href} class="text">{value}</a>
+		{:else}
+			<span class="text">{value}</span>
+		{/if}
 	</div>
 </FormField>
 
-<style>
+<style lang="scss">
 	.input {
-		background-color: transparent;
-		line-height: 2rem;
+		background-color: var(--form-input-disabled-bg, #ccc);
+		color: var(--form-input-fg, #000);
 		font-size: 1rem;
 		width: 100%;
-		padding-left: 1rem;
-		border-bottom: solid 1px var(--form-input-border, black);
+		padding-left: 0.5rem;
+		border: solid 1px var(--form-input-border, black);
+		display: flex;
+		align-items: center;
+		justify-content: flex-start;
+		gap: 0.5rem;
+
+		.icon {
+			display: block;
+			width: 1rem;
+			height: 1rem;
+			position: relative;
+		}
+
+		.text {
+			line-height: 2rem;
+			flex-grow: 1;
+			font-size: 1rem;
+		}
+
+		a {
+			color: var(--form-input-fg, #000);
+			text-decoration: none;
+			display: block;
+
+			&:hover {
+				text-decoration: underline;
+			}
+		}
 	}
 </style>
