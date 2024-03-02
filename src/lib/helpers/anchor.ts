71d5d4c09959747c2a9c1a136525b450b866bd58
Jason Byrne <jasonbyrne662@gmail.com>
2024-03-02 08:52:06 -0500

    v0.0.61

--- src/lib/helpers/anchor.ts
+++ src/lib/helpers/anchor.ts
@@ -1,14 +1,15 @@
 import { browser } from '$app/environment';
 
 export const navigateToAnchor = (anchor: string) => {
-	if (browser) {
-		setTimeout(() => {
-			if (window.top) window.top.location.hash = anchor;
-			else window.location.href = anchor;
-		}, 0);
-	}
+	if (!browser) return;
+	setTimeout(() => {
+		if (window.top) window.top.location.hash = anchor;
+		else window.location.href = anchor;
+	}, 0);
 };
 
 export const getAnchor = (): string | null => {
-	return browser ? window.location.hash : null;
+	if (!browser) return null;
+	const hash = window.top ? window.top.location.hash : window.location.hash;
+	return hash ? hash.slice(1) : null;
 };
